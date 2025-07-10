4a8ad4c112cee4a7a6ae455e086cf6a8f0974ec0
Mr.P <technologymr.p@gmail.com>
2025-07-10 06:13:09 -0300

    add infrastructure

---
+++ Airplane.Best.Routes.Infrastructure/Data/Context/MemoryContext.cs
@@ -0,0 +1,77 @@
+﻿using Airplane.Best.Routes.Domain.Entities;
+using Airplane.Best.Routes.Domain.Interfaces.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Airplane.Best.Routes.Infrastructure.Data.Context
+{
+    public class MemoryContext : DbContext, IMemoryContext
+    {
+        public MemoryContext(DbContextOptions<MemoryContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Route> Routes { get; set; }
+        public DbSet<Connection> Connections { get; set; }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync()
+                .ConfigureAwait(false);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Route>()
+                .HasMany(r => r.Connections)
+                .WithOne(c => c.Route)
+                .HasForeignKey(c => c.RouteId);
+
+            var solutionDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
+            var jsonRoutesFilePath = Path.Combine(solutionDirectory, "Airplane.Best.Routes.Api", "Airplane.Best.Routes.Infrastructure", "Data", "Created", "routes-seed.json");
+
+            if (File.Exists(jsonRoutesFilePath))
+            {
+                var jsonRoutes = File.ReadAllText(jsonRoutesFilePath);
+                var routes = System.Text.Json.JsonSerializer.Deserialize<List<Route>>(jsonRoutes);
+
+                if (routes != null)
+                {
+                    foreach (var route in routes)
+                    {
+                        var routeSeed = new Route
+                        {
+                            Id = route.Id,
+                            OriginName = route.OriginName,
+                            DestinationName = route.DestinationName,
+                            IsAvaiable = route.IsAvaiable,
+                            Value = route.Value
+                        };
+                        modelBuilder.Entity<Route>().HasData(routeSeed);
+                    }
+                }
+            }
+
+            var jsonConnectionsFilePath = Path.Combine(solutionDirectory, "Airplane.Best.Routes.Api", "Airplane.Best.Routes.Infrastructure", "Data", "Created", "connections-seed.json");
+            if (File.Exists(jsonConnectionsFilePath))
+            {
+                var jsonConnections = File.ReadAllText(jsonConnectionsFilePath);
+                var connections = System.Text.Json.JsonSerializer.Deserialize<List<Connection>>(jsonConnections);
+
+                if (connections != null)
+                {
+                    foreach (var connection in connections)
+                    {
+                        var connectionSeed = new Connection
+                        {
+                            Id = connection.Id,
+                            RouteId = connection.RouteId,
+                            Name = connection.Name
+                        };
+                        modelBuilder.Entity<Connection>().HasData(connectionSeed);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Airplane.Best.Routes.Infrastructure/Repositories/RouteRepository.cs
@@ -0,0 +1,109 @@
+﻿using Airplane.Best.Routes.Domain.Entities;
+using Airplane.Best.Routes.Domain.Interfaces.Context;
+using Airplane.Best.Routes.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Airplane.Best.Routes.Infrastructure.Repositories
+{
+    public class RouteRepository : IRouteRepository
+    {
+        private readonly IMemoryContext _memoryContext;
+
+        public RouteRepository(IMemoryContext memoryContext)
+        {
+            _memoryContext = memoryContext;
+        }
+
+        public async Task<List<Route>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return await _memoryContext.Routes
+                .Include(r => r.Connections)
+                .ToListAsync(cancellationToken);
+        }
+
+        public async Task<List<Route>> GetRangeAsync(string originName, string destinationName, CancellationToken cancellationToken)
+        {
+            return await _memoryContext.Routes
+                .Where(r => r.OriginName == originName && r.DestinationName == destinationName)
+                .Include(r => r.Connections)
+                .ToListAsync(cancellationToken);
+        }
+
+        public async Task<Route?> GetBestAsync(string originName, string destinationName, CancellationToken cancellationToken)
+        {
+            return await _memoryContext.Routes
+                .Where(r => r.OriginName == originName && r.DestinationName == destinationName)
+                .OrderBy(r => r.Value)
+                .Include(r => r.Connections)
+                .FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public async Task<Route> CreateAsync(Route route, CancellationToken cancellationToken)
+        {
+            route.Id = Guid.NewGuid();
+            foreach (var connection in route.Connections)
+            {
+                connection.Id = Guid.NewGuid();
+                connection.RouteId = route.Id;
+            }
+
+            var createdRoute = await _memoryContext.Routes.AddAsync(route, cancellationToken);
+            await _memoryContext.SaveChangesAsync();
+
+            return createdRoute.Entity;
+        }
+
+        public async Task<Route?> UpdateAsync(Guid id, Route newRoute)
+        {
+            var dataRoute = await _memoryContext.Routes
+                .Include(r => r.Connections)
+                .FirstOrDefaultAsync(r => r.Id == id);
+
+            if (dataRoute == null)
+                return null;
+
+            dataRoute.OriginName = newRoute.OriginName;
+            dataRoute.DestinationName = newRoute.DestinationName;
+            dataRoute.Value = newRoute.Value;
+
+            if (dataRoute.Connections != null && dataRoute.Connections.Any())
+            {
+                foreach (var conn in dataRoute.Connections.ToList())
+                {
+                    _memoryContext.Connections.Remove(conn);
+                }
+            }
+
+            dataRoute.Connections = new List<Connection>();
+
+            foreach (var connection in newRoute.Connections)
+            {
+                var newConnection = new Connection
+                {
+                    Id = Guid.NewGuid(),
+                    RouteId = dataRoute.Id,
+                    Name = connection.Name
+                };
+                dataRoute.Connections.Add(newConnection);
+                _memoryContext.Connections.Add(newConnection);
+            }
+
+            await _memoryContext.SaveChangesAsync();
+
+            return dataRoute;
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            var route = await _memoryContext.Routes.FirstOrDefaultAsync(r => r.Id == id);
+
+            if (route == null)
+                return false;
+
+            _memoryContext.Routes.Remove(route);
+            await _memoryContext.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
